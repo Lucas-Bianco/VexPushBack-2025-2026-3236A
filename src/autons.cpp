a5a50177ffc865f2cf92b61f4cfb86e2ef37a940
Waffle-Tech-Gaming <Lucasbian8282@gmail.com>
2026-02-11 15:28:01 -0500

    Tweak LeftBlueNonSolo distances and speeds
    Adjust LeftBlueNonSolo autonomous routine: increase initial forward move from 12_in to 16_in; change turn from 90_deg@90 to 100_deg@85; remove the 500ms delay before intake and extend the pickup drive from 24_in@70 to 38_in@40 for a slower, less clog-prone approach. Remove a large block of commented-out legacy sequences to clean up the routine. Rebuilt binaries (.o, .bin, .elf) updated accordingly.

--- src/autons.cpp
+++ src/autons.cpp
@@ -134,10 +134,10 @@ void LeftBlueNonSolo() {
 
 
   // Move Off Start
-  chassis.pid_drive_set(12_in, 100);
+  chassis.pid_drive_set(16_in, 100);
   chassis.pid_wait(); 
   //Turn Right towards Inital 3 Balls
-  chassis.pid_turn_set(90_deg, 90);
+  chassis.pid_turn_set(100_deg, 85);
   chassis.pid_wait();
 
   //Makes Sure Holder is extended
@@ -148,61 +148,13 @@ void LeftBlueNonSolo() {
   //Moves Intake Motors To pickup
   intake.move(-127);
   intakecolorsort.move(-127);
-  //Quick Delay
-  pros::delay(500);
+
   
   //Picks up balls, goes Slowly To be better for Clogs
-  chassis.pid_drive_set(24_in, 70);
+  chassis.pid_drive_set(38_in, 40);
   chassis.pid_wait();
 
-  //Backs Up
-  //chassis.pid_drive_set(-24_in, 90);
-  //chassis.pid_wait();
 
-  //Turns Towards Loader and Long Goal
-  //chassis.pid_turn_set(-90_deg, 90);
-  //chassis.pid_wait();
-
-  //Gets between the Long goal and match loader
-  //chassis.pid_drive_set(24_in, 90);
-  //chassis.pid_wait();
-  //Turns Front Towards match Loader
-  //chassis.pid_turn_set(-90_deg, 90);
-  //chassis.pid_wait();
-  //Puts MatchLoader Down
-  //doinker.set(true);
-  //Gets up against Long Goal
-  //chassis.pid_drive_set(-12_in, 90);
-  //chassis.pid_wait();
-  
-  //Moves Scoring Roller
-  //mainscoring.move(127);
-  //Releaces Balls
-  //holder.set(false);
-
-  //Delay Just In case it takes a while
-  //pros::delay(1000);
-
-  //Rapid Goes Towards Loader
-  //chassis.pid_drive_set(24_in, 90);
-  //chassis.pid_wait();
-
-  //Holds Balls
-  //holder.set(true);
-  //Slower Putting Unloader into the Matchload
-  //chassis.pid_drive_set(10_in, 60);
-  //chassis.pid_wait();
-  //Makes sure they got in 
-  //pros::delay(2000);
-
-  //Go Backwards To Put final blocks in long goal
-  //chassis.pid_drive_set(-24_in, 100);
-  //chassis.pid_wait();
-
-  //Releaces Balls
-  //holder.set(false);
-
-  //Depending on How this works out, can maybe start to hoard some blue balls
   
 }
 
